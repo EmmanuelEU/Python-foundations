9fb9a88970074174739a5b800214e1d7c180ac8c
EmmanuelEU <EmmanuelEU08@gmail.com>
2026-09-02 16:57:40 +0100

    Add files via upload
    Practiced Python data types, including strings, f-strings, booleans, floats and integers

---
+++ data_types.py.py
@@ -0,0 +1,9 @@
+Name = "Emmanuel" 
+print(f"Hello my name is:{Name}") # String: stores text 
+print(f"I am {Age} almost 18") # Integer : stores a whole number 
+am_i_student = True
+print(f"Am i a student that loves tech:{am_i_student}") # This is a boolean which is only has two options true and false 
+f=float(3.50)
+snack = 'haribos'
+print(f"My favourite snack is {snack}. It costs £{f} ") # Float : stores a decimal number 
+
